2b0813a5ffa05c9c2eb531a711eb0c42a98407b6
Your Name <damianstrzelczyk@o2.pl>
2021-03-12 10:33:05 +0100

    solve_1st_part_of_task

---
+++ src/main/java/Domino.java
@@ -0,0 +1,5 @@
+public class Domino {
+    public static final char vertical = '|';
+    public static final char right = '/';
+    public static final char left = '\\';
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,31 @@
 public class Main {
+
+    public static String newStructure(String domino, int iterations){
+
+        char[] dominoesBeforeIteration = domino.toCharArray();
+        char[] dominoesAfterIteration = domino.toCharArray();
+
+        for(int i=0 ; i<iterations;i++){
+            for (int x=0; x<dominoesBeforeIteration.length -1 ;x++){
+                if(dominoesBeforeIteration[x] == Domino.vertical){
+                    if(x>0 && dominoesBeforeIteration[x-1]==Domino.right && x<dominoesBeforeIteration.length-1
+                            && dominoesBeforeIteration[x+1]!=Domino.left){
+                        dominoesAfterIteration[x]=Domino.right;
+                    }
+                    if(x>0 && dominoesBeforeIteration[x+1]==Domino.left && x<dominoesBeforeIteration.length-1
+                            && dominoesBeforeIteration[x-1]!=Domino.right){
+                        dominoesAfterIteration[x]=Domino.left;
+                    }
+                }
+            }
+            dominoesBeforeIteration=dominoesAfterIteration;
+        }
+        String newDominos = new String(dominoesAfterIteration);
+
+        return newDominos;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
